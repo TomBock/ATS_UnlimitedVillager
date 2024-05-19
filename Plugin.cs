f9265607d277b287e4d1ac2c6841a9f7cf133a57
Tom <tom@txkgaming.com>
2024-05-19 21:27:03 +0200

    Initial commit (copy from previous repo)

---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Eremite.Buildings;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace ATSUtils
+{
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource Log => Instance.Logger;
+
+        public static Plugin Instance;
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            Instance = this;
+            harmony = Harmony.CreateAndPatchAll(typeof(Plugin));  
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        
+        [HarmonyPatch(typeof(Building), "PlaceBuilding")]
+        [HarmonyPostfix]
+        private static void HookPlaceBuilding(Building __instance)
+        {
+            if (!Keyboard.current.ctrlKey.isPressed)
+                return;
+
+            Plugin.Log.LogInfo($"Skippping construction process for building {__instance}");
+            
+            _ = new BuildingCreator().CreateCompletedBuilding(
+                __instance.BuildingModel,
+                __instance.BuildingState.field, 
+                __instance.BuildingState.rotation);
+            __instance.Remove(false);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# UnlimitedVillager Mod
+
+Hold CTRL while placing a building to place it immediately without having to wait for 
+its construction and without taking up resources.
